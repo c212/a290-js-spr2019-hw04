78a09f7f06464404809aaf46fb9abfc6469b5ec8
c212 <40395648+c212@users.noreply.github.com>
2019-03-25 10:27:47 -0400

    program version 0
    prints hello

---
+++ 0325/one.js
@@ -0,0 +1,11 @@
+/* A school’s lockers are numbered 1 to 100. One hundred students
+enter the school one at a time. The first student opens the lockers.
+The second student closes the even numbered lockers. The third
+student either closes or opens every third locker. The remaining
+students continue the pattern. Write a program to determine
+which lockers remain open at the end, after the last one of the
+students passes. Explain the result. Make sure the program not
+only reports the situation at the end but also conveys the stages
+through which the array of lockers passes. */ 
+
+console.log( "So, what do you think?! ... " ); 
